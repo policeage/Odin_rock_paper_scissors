43ce8dfff4cdd5659f52286f1a2022a20789f269
policeage <robh1742@gmail.com>
2022-08-18 19:03:33 -0700

    Added Comments to JS

--- assets/js/myScripts.js
+++ assets/js/myScripts.js
@@ -1,3 +1,4 @@
+//global variables
 let cpuChoice;
 let playerChoice;
 let winCount = 0;
@@ -5,8 +6,11 @@ let loseCount = 0;
 let tieCount = 0;
 const choice = ["ROCK","PAPER","SCISSORS"];
 
+//This starts the playfivegames on load, just delete if you don't want it to start immediately and want to start it via console instead
 playfivegames()
 
+//This was the original start option, it might not be used in the future but currently
+//it is not used at all
 function gameTime() {
     let gameConfirm = confirm("Do you want to play a game?");
     if (gameConfirm==false) {
@@ -20,7 +24,7 @@ function gameTime() {
     }
 }
 
-
+//sends prompt to set playerChoice variable, I couldn't figure out how to embed the variables inside the function
 function getPlayerChoice() {
     let playerSelect = prompt('Please Enter Your Choice:', "Rock, Paper, Scissors");
     playerChoice = playerSelect.toUpperCase();
@@ -31,13 +35,18 @@ function getPlayerChoice() {
     } 
 }  
 
+//randoms a choice from the choice array and assigns it to the cpuChoice global variable
+
 function getComputerChoice() {
     result = choice[Math.floor(Math.random()*choice.length)];
     cpuChoice = result.toUpperCase()
     return console.log('CPU output:', cpuChoice);
 }
 
-
+//Checks the playerChoice against the cpuChoice, outputs a window to confirm if you want to play again
+//In practice, the playfivegames is it's own loop but it can be broken if you say no, but it'll also only play five games.
+//Additionally, the win, loss, and ties add a count to their global variables and is displayed after the iteration is completed
+//the total display is only available from a completed iteration, will have to change later
 function getGameResults() {
     if (playerChoice == cpuChoice) {
             let playAgainTie = confirm(`It\'s a tie? Now who let that happen? I\'m sure you can guess you both played ${playerChoice} Play again?`); {
@@ -71,6 +80,7 @@ function getGameResults() {
     }
 }
 
+//Current playfivegames function, it's on start of the load page, it also displays the total results from all played games.
 function playfivegames() {
   for (let i = 0; i < 5; i++) {
     getComputerChoice();
